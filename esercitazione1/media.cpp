3ae9a501806ef952c7af44054af4dd497e77db9e
concettagifuni-17 <concettagifuni@gmail.com>
2026-03-16 19:40:32 +0000

    perfezionato i messaggi di errore: sostituito 'cout' con 'cerr', aggiunto 'endl' per maggiore leggibilita, sostituito il secondo messaggio di errore

--- esercitazione1/media.cpp
+++ esercitazione1/media.cpp
@@ -1,34 +1,34 @@
-#include <iostream> 
+#include <iostream>
 #include <fstream>
 using namespace std;
 
-int main(int argc, const char *argv[]) {
-    
-    if (argc < 2) {
-        cout <<"Errore: nome del file non specificato";
+int main(int argc, const char * argv[]) {
+
+    if (argc<2) {
+        cerr << "Errore: nome del file non specificato" << endl;
         return 1;
     }
-    
+   
     fstream ifs(argv[1]);
 
     if (!ifs.is_open()) {
-        cout <<"Errore: impossibile aprire il file";
+        cerr <<"Errore: impossibile aprire il file correttamente" << endl;
         return 1;
     }
-    
+
     string location;
     double t1, t2, t3, t4;
     double ntemp = 4.0;
 
-    while (ifs >> location >> t1 >> t2>> t3 >> t4) {
-        double media = (t1+t2+t3+t4)/ntemp;
+    while (ifs >> location >> t1 >> t2 >> t3 >> t4) {
+        double media = (t1 + t2 +t3 + t4)/ ntemp;
         cout << location;
-        int nspazi = 12 - location.size();
-        for (int i = 0; i < nspazi; i++) {
+        
+        for (int i =0; i < 12 - location.size(); i++) {
             cout << " ";
         }
-        cout << media << endl;
+        cout << media <<endl;
     }
- 
+
     return 0;
 }
