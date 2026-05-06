846c9ea0856b7089d012661c9414e92a1d086ac1
concettagifuni-17 <concettagifuni@gmail.com>
2026-05-06 15:57:05 +0000

    consegna di alcuni file dell'esercitazione

---
+++ esercitazione5/test_quicksort.cpp
@@ -0,0 +1,34 @@
+#include <cstdlib>
+#include <vector>
+#include <string>
+using namespace std;
+
+#include "sorts.hpp"
+#include "randfiller.h"
+
+int main() {
+    randfiller rf;
+    for (int i = 0; i < 100; i++) {
+        vector<int> n(1);
+        rf.fill(n, 0, 100);
+        int N = n[0];
+        vector<int> vec(N);
+        rf.fill(vec, -1000, 1000);
+        if (vec.size() > 0){
+            quick_sort(vec, 0, vec.size() - 1);
+        }
+        if (!is_sorted(vec)) {
+            return EXIT_FAILURE;
+        }
+    }
+    vector<string> str = {"gioiello", "nuvoloso", "gatto", "ventilatore", "centrifugare", "infermiera", "elettrico", "schermo", "aquile", "spazio"};
+    
+    if (str.size() > 0) {
+    quick_sort(str, 0, str.size() - 1);
+    }
+    if (!is_sorted(str)) {
+        return EXIT_FAILURE;
+    }
+    
+    return EXIT_SUCCESS;
+}
